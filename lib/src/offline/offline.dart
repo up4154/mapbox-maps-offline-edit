0fd7f72c9e447094e3c222c89b24e7630b85ab50
utsav.porwal <utsavporwal01@gmail.com>
2023-08-30 12:34:01 +0530

    app was running

--- lib/src/offline/offline.dart
+++ lib/src/offline/offline.dart
@@ -3,6 +3,7 @@ part of mapbox_maps_flutter;
 
 extension OfflineLoading on OfflineManager{
   Future<void> loadOfflineData() {
+    print("loadOfflinedata called");
     return cacheMapLayer();
   }
 }
