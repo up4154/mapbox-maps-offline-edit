4ca779a961192bf55f01f95c2d7cb5f4364b12b6
utsav.porwal <utsavporwal01@gmail.com>
2023-08-30 14:32:25 +0530

    app was running

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineController.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineController.kt
@@ -72,7 +72,7 @@ class OfflineController(private val mapView: MapView):FLTMapInterfaces.OfflineMa
         println("Downloaded SuccessFully")
       }
     }
-result.success(null)
+result.success(it.coordinateJson.toString)
   }
 
 
