01bdade59d5f2295f44aca09ccba29e42c16eb2f
utsav.porwal <utsavporwal01@gmail.com>
2023-08-31 10:50:35 +0530

    app was running

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineController.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineController.kt
@@ -66,7 +66,7 @@ class OfflineController(private val mapView: MapView):FLTMapInterfaces.OfflineMa
         }
     """.trimIndent()
   val tileRegionId = "Some Random String"
-  override  suspend fun cacheMapLayer(result: FLTMapInterfaces.Result<String>){
+  suspend override   fun cacheMapLayer(result: FLTMapInterfaces.Result<String>){
     println("cache map layer in called in offline controller")
 
     val tileRegionLoadOptions = TileRegionLoadOptions.Builder()
