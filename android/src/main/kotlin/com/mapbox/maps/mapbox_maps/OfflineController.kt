f3e214a44a23650d95c67d63ff42f58aaab853e6
utsav.porwal <utsavporwal01@gmail.com>
2023-09-01 10:51:12 +0530

    tile region list added

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineController.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineController.kt
@@ -108,7 +108,7 @@ class OfflineController(private val mapView: MapView):FLTMapInterfaces.OfflineMa
         println("TileRegionError: $tileRegionError")
       }
     }
-result.success(tileRegionList.toString())
+result.success(tileRegionExisting.toString())
   }
 
 
