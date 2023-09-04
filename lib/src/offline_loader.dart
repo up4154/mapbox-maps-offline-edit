eb46b4f5da039003f890d88e1f3f06d52146b621
utsav.porwal <utsavporwal01@gmail.com>
2023-09-04 17:01:56 +0530

    offline handling

--- lib/src/offline_loader.dart
+++ lib/src/offline_loader.dart
@@ -1,20 +1,20 @@
-part of mapbox_maps_flutter;
-
-
-// late final BinaryMessenger? _binaryMessenger;
-MethodChannel _globalChannel =
-MethodChannel("offline-cacheMapLayer");
-
-Future<String> cacheMapLayer() async {
-  print("cache map layer is called in offlineLoader.dart");
-
-  try {
-    final String result =
-    await _globalChannel.invokeMethod('cacheMapLayer');
-    print('Result from Kotlin: $result');
-    return result;
-  } catch (e) {
-    print('Error invoking method: $e');
-    return "";
-  }
-}
+// part of mapbox_maps_flutter;
+//
+//
+// // late final BinaryMessenger? _binaryMessenger;
+// MethodChannel _globalChannel =
+// MethodChannel("offline-cacheMapLayer");
+//
+// Future<String> cacheMapLayer() async {
+//   print("cache map layer is called in offlineLoader.dart");
+//
+//   try {
+//     final String result =
+//     await _globalChannel.invokeMethod('cacheMapLayer');
+//     print('Result from Kotlin: $result');
+//     return result;
+//   } catch (e) {
+//     print('Error invoking method: $e');
+//     return "";
+//   }
+// }
