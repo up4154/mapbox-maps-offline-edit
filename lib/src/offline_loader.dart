ac32a735766c0431ce1639527207dff558e43266
utsav.porwal <utsavporwal01@gmail.com>
2023-09-04 15:49:25 +0530

    offline handling

--- lib/src/offline_loader.dart
+++ lib/src/offline_loader.dart
@@ -2,11 +2,12 @@ part of mapbox_maps_flutter;
 
 
 // late final BinaryMessenger? _binaryMessenger;
+ MethodChannel _globalChannel =
+MethodChannel("dev.flutter.pigeon.OfflineManager.cacheMapLayer");
 
 Future<String> cacheMapLayer() async {
   print("cache map layer is called in map_interface.dart");
-  final MethodChannel _globalChannel =
-  MethodChannel("dev.flutter.pigeon.OfflineManager.cacheMapLayer");
+
   try {
     final String result =
     await _globalChannel.invokeMethod('cacheMapLayer');
