ad9bc31f3131aea964aea7e7445cf62f6a460712
utsav.porwal <utsavporwal01@gmail.com>
2023-09-05 12:57:04 +0530

    offline loader added

--- lib/src/offline_loader.dart
+++ lib/src/offline_loader.dart
@@ -4,10 +4,11 @@ part of mapbox_maps_flutter;
 final MethodChannel _offlineChannel= MethodChannel("offline_method_channel");
 
 
-Future<void>getOfflineRegion() async{
+Future<String>getOfflineRegion() async{
   final result = await _offlineChannel.invokeMethod("cacheMapLayer");
   if(result==null){
     print("result is null");
   }
   print(result);
+  return(result);
 }
