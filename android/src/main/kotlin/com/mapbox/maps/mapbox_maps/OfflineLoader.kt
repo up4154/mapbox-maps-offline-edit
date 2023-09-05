c61df315c1896b068abd836e08d58e1ba893dc00
utsav.porwal <utsavporwal01@gmail.com>
2023-09-05 14:48:45 +0530

    offline loader added

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
@@ -41,7 +41,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 
-class OfflineLoader: FlutterActivity(){
+class MainActivity: FlutterActivity(){
     val channel_name="offline_method_channel"
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
     super.configureFlutterEngine(flutterEngine)
@@ -50,82 +50,82 @@ class OfflineLoader: FlutterActivity(){
       channel_name
     ).setMethodCallHandler { call, result ->
       if (call.method == "cacheMapLayer") {
-        cacheMapLayer()
+//        cacheMapLayer()
         result.success("cache map layer called in  offline loader in kotlin")
       } else {
         result.notImplemented()
       }
     }
   }
-  private val mapView: MapView = MapView(this)
-  private var offlineManager: OfflineManager = OfflineManager(MapInitOptions.getDefaultResourceOptions(mapView.context))
-  private var tilesetDescriptorForStyle: TilesetDescriptor = offlineManager.createTilesetDescriptor(
-    TilesetDescriptorOptions.Builder()
-      .styleURI(Style.OUTDOORS)
-      .minZoom(0)
-      .maxZoom(16)
-      .build()
-  )
-  private val tileStore = TileStore.create().also {
-    it.setOption(
-      TileStoreOptions.MAPBOX_ACCESS_TOKEN,
-      TileDataDomain.MAPS,
-      Value(mapView.context.getString(R.string.mapbox_access_token))
-
-    )
-  }
-  private val polygonJsonString = """
-    {
-        "type": "Polygon",
-        "coordinates": [
-             [
-                    [-122.41857910156249, 37.76023484134737],
-                    [-122.42177963256836, 37.75422679365761],
-                    [-122.40983009338379, 37.75363563119239],
-                    [-122.40655899047852, 37.75897167811799],
-                    [-122.41857910156249, 37.76023484134737]
-                ]
-            ]
-        
-    }
-""".trimIndent()
-
-  val tileRegionId = "Some Random String"
-  fun cacheMapLayer(){
-    println("cache map layer in called in offline controller")
-
-    val tileRegionLoadOptions = TileRegionLoadOptions.Builder()
-      .geometry(Polygon.fromJson(polygonJsonString))
-      .descriptors(listOf( tilesetDescriptorForStyle))
-      .acceptExpired(true)
-      .networkRestriction(NetworkRestriction.NONE)
-      .build()
-    val tileRegionCancelable = tileStore.loadTileRegion(
-      tileRegionId,
-      tileRegionLoadOptions,
-      { progress ->
-        println("$progress")
-      }
-    ) { expected ->
-      if (expected.isValue) {
-        println("Downloaded SuccessFully")
-      }
-      else{
-        println("downloading not completed")
-      }
-    }
-//    tileRegionCancelable.cancel()
-    val tileRegionExisting =  tileStore.getAllTileRegions { expected ->
-      if (expected.isValue) {
-        expected.value?.let { tileRegionList ->
-          println("Existing tile regions: $tileRegionList")
-
-        }
-      }
-      expected.error?.let { tileRegionError ->
-        println("TileRegionError: $tileRegionError")
-      }
-    }
-
-  }
+//  private val mapView: MapView = MapView(this)
+//  private var offlineManager: OfflineManager = OfflineManager(MapInitOptions.getDefaultResourceOptions(mapView.context))
+//  private var tilesetDescriptorForStyle: TilesetDescriptor = offlineManager.createTilesetDescriptor(
+//    TilesetDescriptorOptions.Builder()
+//      .styleURI(Style.OUTDOORS)
+//      .minZoom(0)
+//      .maxZoom(16)
+//      .build()
+//  )
+//  private val tileStore = TileStore.create().also {
+//    it.setOption(
+//      TileStoreOptions.MAPBOX_ACCESS_TOKEN,
+//      TileDataDomain.MAPS,
+//      Value(mapView.context.getString(R.string.mapbox_access_token))
+//
+//    )
+//  }
+//  private val polygonJsonString = """
+//    {
+//        "type": "Polygon",
+//        "coordinates": [
+//             [
+//                    [-122.41857910156249, 37.76023484134737],
+//                    [-122.42177963256836, 37.75422679365761],
+//                    [-122.40983009338379, 37.75363563119239],
+//                    [-122.40655899047852, 37.75897167811799],
+//                    [-122.41857910156249, 37.76023484134737]
+//                ]
+//            ]
+//
+//    }
+//""".trimIndent()
+//
+//  val tileRegionId = "Some Random String"
+//  fun cacheMapLayer(){
+//    println("cache map layer in called in offline controller")
+//
+//    val tileRegionLoadOptions = TileRegionLoadOptions.Builder()
+//      .geometry(Polygon.fromJson(polygonJsonString))
+//      .descriptors(listOf( tilesetDescriptorForStyle))
+//      .acceptExpired(true)
+//      .networkRestriction(NetworkRestriction.NONE)
+//      .build()
+//    val tileRegionCancelable = tileStore.loadTileRegion(
+//      tileRegionId,
+//      tileRegionLoadOptions,
+//      { progress ->
+//        println("$progress")
+//      }
+//    ) { expected ->
+//      if (expected.isValue) {
+//        println("Downloaded SuccessFully")
+//      }
+//      else{
+//        println("downloading not completed")
+//      }
+//    }
+////    tileRegionCancelable.cancel()
+//    val tileRegionExisting =  tileStore.getAllTileRegions { expected ->
+//      if (expected.isValue) {
+//        expected.value?.let { tileRegionList ->
+//          println("Existing tile regions: $tileRegionList")
+//
+//        }
+//      }
+//      expected.error?.let { tileRegionError ->
+//        println("TileRegionError: $tileRegionError")
+//      }
+//    }
+//
+//  }
 }
