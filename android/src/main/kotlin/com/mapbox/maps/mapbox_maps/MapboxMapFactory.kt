96e27b618dc434c6147cbd6b776f4db59e937486
utsav.porwal <utsavporwal01@gmail.com>
2023-09-06 12:25:01 +0530

    offline loader added

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/MapboxMapFactory.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/MapboxMapFactory.kt
@@ -117,7 +117,7 @@ class MapboxMapFactory(
     val channelSuffix = params["channelSuffix"] as Int
 
     val textureView = params["textureView"] as? Boolean ?: false
-    val styleUri = params["styleUri"] as? String
+    val styleUri = params["styleUri"] as? String ?: Style.MAPBOX_STREETS
     val pluginVersion = params["mapboxPluginVersion"] as String
     val mapInitOptions = MapInitOptions(
       context = context,
