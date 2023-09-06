ae3b24995bcf72cb489344213fd53ab853d3273c
utsav.porwal <utsavporwal01@gmail.com>
2023-09-06 16:32:19 +0530

    offline layer added successfully

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
@@ -131,7 +131,8 @@ class OfflineLoader{
       .build()
     val tileStyleLoadOptions = StylePackLoadOptions.Builder()
 //      .acceptExpired(true)
-      .glyphsRasterizationMode(GlyphsRasterizationMode.NO_GLYPHS_RASTERIZED_LOCALLY
+      .glyphsRasterizationMode(GlyphsRasterizationMode.IDEOGRAPHS_RASTERIZED_LOCALLY
+
       )
       .build()
     val tileRegionCancelable = tileStore.loadTileRegion(
