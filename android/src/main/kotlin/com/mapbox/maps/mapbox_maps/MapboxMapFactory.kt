c51eb83deeae6ae205310d947eda5b69c7c76959
utsav.porwal <utsavporwal01@gmail.com>
2023-09-07 12:29:22 +0530

    woroking for mapbox _streets

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/MapboxMapFactory.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/MapboxMapFactory.kt
@@ -117,7 +117,7 @@ class MapboxMapFactory(
     val channelSuffix = params["channelSuffix"] as Int
 
     val textureView = params["textureView"] as? Boolean ?: false
-    val styleUri = params["styleUri"] as? String ?: Style.SATELLITE_STREETS
+    val styleUri = params["styleUri"] as? String ?: Style.SATELLITE
     val pluginVersion = params["mapboxPluginVersion"] as String
     val mapInitOptions = MapInitOptions(
       context = context,
--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
@@ -77,7 +77,7 @@ class OfflineLoader{
     )
     var tilesetDescriptorForStyle: TilesetDescriptor = offlineManager.createTilesetDescriptor(
       TilesetDescriptorOptions.Builder()
-        .styleURI(Style.MAPBOX_STREETS)
+        .styleURI(Style.SATELLITE)
         .minZoom(0)
         .maxZoom(22)
         .build()
@@ -146,7 +146,7 @@ class OfflineLoader{
         if(expected.value?.completedResourceCount == expected.value?.requiredResourceCount) {
           val stylePackCancelable = offlineManager.loadStylePack(
 
-            Style.MAPBOX_STREETS,
+            Style.SATELLITE,
             // Build Style pack load options
             tileStyleLoadOptions,
             { progress ->
