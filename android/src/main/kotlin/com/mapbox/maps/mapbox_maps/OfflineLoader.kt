536438ad390e10e0e2cac8c2642a90ca0504b3da
utsav.porwal <utsavporwal01@gmail.com>
2023-09-07 11:50:05 +0530

    woroking for mapbox _streets

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
@@ -65,7 +65,7 @@ class OfflineLoader{
   {
     println("cache map layer in called in offline controller")
     val tileUrlList = arrayListOf<String>()
-    tileUrlList.add("mapbox://mapbox.mapbox-traffic-v1")
+//    tileUrlList.add("mapbox://mapbox.mapbox-traffic-v1")
     tileUrlList.add("mapbox://mapbox.mapbox-terrain-v2")
     var offlineManager: OfflineManager = OfflineManager(MapInitOptions.getDefaultResourceOptions(context))
     var tilesetDescriptorLines: TilesetDescriptor = offlineManager.createTilesetDescriptor(
@@ -77,7 +77,7 @@ class OfflineLoader{
     )
     var tilesetDescriptorForStyle: TilesetDescriptor = offlineManager.createTilesetDescriptor(
       TilesetDescriptorOptions.Builder()
-        .styleURI(Style.SATELLITE_STREETS)
+        .styleURI(Style.MAPBOX_STREETS)
         .minZoom(0)
         .maxZoom(22)
         .build()
@@ -146,7 +146,7 @@ class OfflineLoader{
         if(expected.value?.completedResourceCount == expected.value?.requiredResourceCount) {
           val stylePackCancelable = offlineManager.loadStylePack(
 
-            Style.SATELLITE_STREETS,
+            Style.MAPBOX_STREETS,
             // Build Style pack load options
             tileStyleLoadOptions,
             { progress ->
