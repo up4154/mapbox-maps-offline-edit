01e5473a700dd4d4ae6782a6a579945300e69432
utsav.porwal <utsavporwal01@gmail.com>
2023-09-08 11:19:15 +0530

    woroking for mapbox _streets

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/OfflineLoader.kt
@@ -148,7 +148,11 @@ class OfflineLoader{
 //      .acceptExpired(true)
 //      .networkRestriction(NetworkRestriction.NONE)
 //      .build()
-
+    val tileStyleLoadOptions = StylePackLoadOptions.Builder()
+      .acceptExpired(true)
+      .glyphsRasterizationMode(GlyphsRasterizationMode.IDEOGRAPHS_RASTERIZED_LOCALLY
+      )
+      .build()
     val tileRegionCancelable = tileStore.loadTileRegion(
       tileRegionId,
       tileRegionLoadOptions,
