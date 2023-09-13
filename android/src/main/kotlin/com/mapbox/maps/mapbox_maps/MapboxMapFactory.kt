47a49bf237251f081a527aae132bb7c7dd0cdf34
utsav.porwal <utsavporwal01@gmail.com>
2023-09-13 17:40:15 +0530

    woroking for mapbox _streets

--- android/src/main/kotlin/com/mapbox/maps/mapbox_maps/MapboxMapFactory.kt
+++ android/src/main/kotlin/com/mapbox/maps/mapbox_maps/MapboxMapFactory.kt
@@ -25,6 +25,7 @@ class MapboxMapFactory(
     (params["resourceOptions"] as Map<String, Any>?)?.let { resourceOptions ->
       resourceOptions["accessToken"]?.let {
         resourceOptionsBuilder.accessToken(it as String)
+
       }
       resourceOptions["baseURL"]?.let {
         resourceOptionsBuilder.baseURL(it as String)
